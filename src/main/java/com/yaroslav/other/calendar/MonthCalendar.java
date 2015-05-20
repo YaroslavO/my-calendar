370d91a9f8958c99517351abe7b00b2f449ed90a
motomen <gdpdp.ru@mail.ru>
2015-05-20 20:26:12 +0300

    complete MonthCalendar class

--- src/main/java/com/yaroslav/other/calendar/MonthCalendar.java
+++ src/main/java/com/yaroslav/other/calendar/MonthCalendar.java
@@ -18,10 +18,32 @@ public class MonthCalendar {
 
     public void init(Calendar date) {
         this.date = date;
+        Week currentWeek;
+        for (int count = 1; count <= getCountWeeks(date); count++) {
+            currentWeek = new Week();
+            if (count == 1) {
+                currentWeek.init(date);
+            } else {
+                currentWeek = weeks.get(count - 1).createNextWeek();
+            }
+            weeks.add(currentWeek);
+        }
     }
 
     @Override
     public String toString() {
         return weeks.toString();
+    }
+
+    public int getCountWeeks(Calendar calendar) {
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+        int cntDayOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        int countWeek = (dayOfWeek + cntDayOfMonth) / 7;
+
+        if (((dayOfWeek + cntDayOfMonth) % 7) != 0) {
+            countWeek += 1;
+        }
+
+        return countWeek;
     }
 }
