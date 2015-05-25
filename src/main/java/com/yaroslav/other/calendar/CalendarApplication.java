147f5b9b251ff57bae9ccd383798e4a3b7ecfdff
Yaroslav Olenenko <jaroslavo@softwareplanet.uk.com>
2015-05-25 18:21:37 +0300

    finish create customer calendar

--- src/main/java/com/yaroslav/other/calendar/CalendarApplication.java
+++ src/main/java/com/yaroslav/other/calendar/CalendarApplication.java
@@ -1,9 +1,12 @@
 package com.yaroslav.other.calendar;
 
+import com.yaroslav.other.calendar.file.SourceLoader;
 import com.yaroslav.other.calendar.view.ConsoleAbstractCalendarMonthCalendarRenderer;
 import com.yaroslav.other.calendar.view.HTMLAbstractCalendarMonthCalendarRenderer;
 import com.yaroslav.other.calendar.view.MonthCalendarRenderer;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.stream.Collectors;
 
@@ -14,7 +17,7 @@ public class CalendarApplication {
     private static final String COMMAND_OUT = "--out";
     private static final String COMMAND_TEXT = "[text]";
     private static final String COMMAND_CONSOLE = "[console]";
-    private static final String FILE_NAME = "calendar.html";
+    private static final String FILE_NAME = "calendar.txt";
 
     public static void main(String[] args) {
         InputReader inputReader = new InputReader();
@@ -38,5 +41,20 @@ public class CalendarApplication {
                 System.out.println(monthCalendarRenderer.render(monthCalendar));
             }
         }
+
+        SourceLoader sourceLoader = new SourceLoader();
+        String source = "2015 jul";
+
+        try {
+            source = sourceLoader.loadSource(FILE_NAME);
+        }  catch (FileNotFoundException e) {
+            System.out.println("file not found: " + FILE_NAME);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        CustomerCalendar calendar = new CustomerCalendar(source);
+        calendar.init();
+
     }
 }
--- src/main/java/com/yaroslav/other/calendar/FileManager.java
+++ src/main/java/com/yaroslav/other/calendar/FileManager.java
@@ -1,6 +1,7 @@
 package com.yaroslav.other.calendar;
 
 import java.io.*;
+import java.nio.file.Files;
 
 /**
  * Created by Yaroslav on 21.05.2015.
@@ -19,6 +20,22 @@ public class FileManager {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public String readFile(String fileName) {
+
+        return " ";
+    }
+
+    private void createUploadDirIfNotExist(String pathToDir) {
+        File dir = new File(pathToDir);
+        if (!dir.exists()) {
+            try {
+                Files.createDirectories(dir.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
